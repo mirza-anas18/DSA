c9ea814f938175216952bb84458be9db39a21ce3
mirza anas baig <anasbaig7636@gmail.com>
2025-08-19 21:42:31 +0530

    Add files via upload

---
+++ numberofdigits.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class numberofdigits{
+    public static void main(String args[]){
+        int n=444;
+        int count=0;
+        
+        while(n>0){
+            int lastdigit=n%10;
+            count=count+1;
+            n=n/10;
+        }
+        System.out.println("Number of digits: "+count);
+    }
+}
